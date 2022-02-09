d450cd9d0f2e759e5766625522b1f773ce68a3aa
Sean Mollet <sean@malmoset.com>
2022-02-09 18:48:20 +0000

    48000 isn't supported, went back to 44100

--- src/platform/hi3516cv500/himpp-audio.h
+++ src/platform/hi3516cv500/himpp-audio.h
@@ -45,7 +45,7 @@ class HimppAencChan;
 class HimppAudioCodec : public HimppAudioElement, public AudioSource 
 {
 public:
-	HimppAudioCodec(AUDIO_SAMPLE_RATE_E sample_rate = AUDIO_SAMPLE_RATE_48000);
+	HimppAudioCodec(AUDIO_SAMPLE_RATE_E sample_rate = AUDIO_SAMPLE_RATE_44100);
 	~HimppAudioCodec();
 
 	// implementation of AudioSource
