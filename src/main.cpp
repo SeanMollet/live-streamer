89d663b2108f6bc6493ef5fac6ac5ccde0f43f5c
Sean Mollet <sean@malmoset.com>
2022-02-09 21:18:03 +0000

    Added a log line notifying us that we've loaded the configuration file

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,17 @@
 /*
  * main.cpp
  * Copyright (C) 2015 Watson Xu <xuhuashan@gmail.com>
- * 
+ *
  * rtsp-streamer is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * rtsp-streamer is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -270,6 +270,7 @@ int main(int argc, char *argv[])
 #endif
 
 #ifdef HAVE_JSONCPP_SUPPORT
+	fprintf(stderr, "Loading configuration file\n");
 	runtime->LoadConfig();
 #endif
 
